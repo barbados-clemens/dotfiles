aaad1ca0b7ee6e85820c2903ccf41bfbfa36b0a1
Caleb Ukle <caleb.ukle+github@pm.me>
2025-11-01 13:00:54 -0500

    use wordwrap

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Enable word wrapping for long lines
+vim.opt.wrap = true         -- Enable line wrapping
+vim.opt.linebreak = true    -- Break lines at word boundaries
+vim.opt.breakindent = true  -- Preserve indentation on wrapped lines
